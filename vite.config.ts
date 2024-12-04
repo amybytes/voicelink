a16f057e722bdec66eed018924948191ad295d4b
amybytes <amy@amybytes.com>
2024-12-04 16:13:18 -0500

    Add base url to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,35 @@
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
+interface ConfigInput {
+  mode: string;
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    host: '0.0.0.0',
-  },
-  resolve: {
-    alias: {
-      src: '/src/',
-      components: '/src/components',
-      contexts: '/src/contexts',
-      hooks: '/src/hooks',
-      routes: '/src/routes',
-      api: '/src/api',
-      assets: '/src/assets',
-      images: '/src/assets/images',
-      icons: '/src/assets/icons',
-      styles: '/src/assets/styles',
-      constants: '/src/assets/constants',
+export default ({mode}: ConfigInput) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react(), svgr()],
+    base: '/' + process.env.VITE_BASE_URL,
+    server: {
+      host: '0.0.0.0',
     },
-  },
-});
+    resolve: {
+      alias: {
+        src: '/src/',
+        components: '/src/components',
+        contexts: '/src/contexts',
+        hooks: '/src/hooks',
+        routes: '/src/routes',
+        api: '/src/api',
+        assets: '/src/assets',
+        images: '/src/assets/images',
+        icons: '/src/assets/icons',
+        styles: '/src/assets/styles',
+        constants: '/src/assets/constants',
+      },
+    },
+  });
+};
