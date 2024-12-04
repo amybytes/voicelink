f21daeb31ddef03fe5eb06e9b13e3a5d2db8231f
amybytes <amy@amybytes.com>
2024-12-04 14:09:12 -0500

    Fix settings at root

--- src/components/sidebar/Sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar/Sidebar.tsx
@@ -1,11 +1,11 @@
-import "./Sidebar.css";
+import './Sidebar.css';
 import {useState} from 'react';
 import clsx from 'clsx';
-import SidebarButton from "components/sidebar/SidebarButton/SidebarButton";
-import SidebarHeader from "components/sidebar/SidebarHeader/SidebarHeader";
-import SidebarMenu from "components/sidebar/SidebarMenu/SidebarMenu";
-import SidebarSettings from "components/sidebar/SidebarSettings/SidebarSettings";
-import SidebarFooter from "components/sidebar/SidebarFooter/SidebarFooter";
+import SidebarButton from 'components/sidebar/SidebarButton/SidebarButton';
+import SidebarHeader from 'components/sidebar/SidebarHeader/SidebarHeader';
+import SidebarMenu from 'components/sidebar/SidebarMenu/SidebarMenu';
+import SidebarSettings from 'components/sidebar/SidebarSettings/SidebarSettings';
+import SidebarFooter from 'components/sidebar/SidebarFooter/SidebarFooter';
 
 export default function Sidebar() {
   const [open, setOpen] = useState(false);
@@ -15,12 +15,13 @@ export default function Sidebar() {
   }
 
   return (
-    <div className={clsx("sidebar", open ? 'open' : '')}>
+    <div className={clsx('sidebar', open ? 'open' : '')}>
       <SidebarButton open={open} onClick={handleClick} />
       <SidebarHeader />
       <SidebarMenu onNavigate={() => setOpen(false)} />
+      {/* TODO: Add a section to toggle hovering box for text/reading */}
       <SidebarSettings />
       <SidebarFooter />
     </div>
-  )
+  );
 }
--- src/components/sidebar/SidebarSettings/SidebarSettings.tsx
+++ src/components/sidebar/SidebarSettings/SidebarSettings.tsx
@@ -1,15 +1,15 @@
-import {PAGE_PATHS} from "src/assets/constants/app";
-import "./SidebarSettings.css";
-import {useLocation} from "react-router-dom";
+import {PAGE_PATHS, PAGES} from 'src/assets/constants/app';
+import './SidebarSettings.css';
+import {useLocation} from 'react-router-dom';
 
 export default function SidebarSettings() {
   const location = useLocation();
-  const page = location ? PAGE_PATHS[location.pathname] : null;
+  const page = PAGE_PATHS[location?.pathname] ?? PAGES.PITCH_MONITOR;
 
   return (
     <div className="sidebar-settings">
       <h2>Settings</h2>
       {page && <page.Settings />}
     </div>
-  )
+  );
 }
